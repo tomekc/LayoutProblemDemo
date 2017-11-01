1b5e93e66597b56c7a308f2800ca2c2e5cb8b74c
Tomek Cejner <tomek@smartrecruiters.com>
2017-11-01 20:16:06 +0100

    Initial commit

---
+++ LayoutProblemDemo/DelayedTimePickerCell.swift
@@ -0,0 +1,24 @@
+//
+//  DelayedTimePickerCell.swift
+//  BackOffice
+//
+//  Created by Tomek on 30/10/2017.
+//  Copyright © 2017 SmartRecruiters. All rights reserved.
+//
+
+import UIKit
+
+class DelayedTimePickerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- LayoutProblemDemo/ViewController.swift
+++ LayoutProblemDemo/ViewController.swift
@@ -7,18 +7,55 @@
 //
 
 import UIKit
+import Layout
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tview: UITableView!
+    
+    
+    enum CellIdentifiers {
+        static let TimePicker = "DelayedTimePicker"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tview.rowHeight = UITableViewAutomaticDimension
+        tview.estimatedRowHeight = 55.0
+        
+        tview.registerLayout(named: "DelayedTimePickerCell.xml", forCellReuseIdentifier: CellIdentifiers.TimePicker)
+        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let node = tableView.dequeueReusableCellNode(withIdentifier: CellIdentifiers.TimePicker, for: indexPath)
+        node.setState([])
+        return node.view as! UITableViewCell
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
 
 }
